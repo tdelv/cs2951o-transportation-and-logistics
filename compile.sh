3ffca0a9d7e06364438b009fdf13984d6bb644d5
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2021-04-03 00:31:35 -0400

    Update to gitignore?

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-########################################
-############# CSCI 2951-O ##############
-########################################
-
-# Update this file with instructions on how to compile your code
-javac -classpath /gpfs/main/sys/shared/psfu/local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cpoptimizer/lib/ILOG.CP.jar;/gpfs/main/sys/shared/psfu/local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cplex/lib/cplex.jar ./src/solver/ls/*.java
